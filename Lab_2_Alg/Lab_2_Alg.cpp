f26dc11419da9286a6772a73bbe5bc9a81d129b1
Shishkov Egor <131530870+AnimeMakartur@users.noreply.github.com>
2026-02-24 12:12:06 +0200

    dsf

--- Lab_2_Alg/Lab_2_Alg.cpp
+++ Lab_2_Alg/Lab_2_Alg.cpp
@@ -51,7 +51,7 @@ void printWays(int IsCycle) {
     printf("--------------------------------\n");
     printf("N\t|Values\n");
     printf("--------------------------------\n");
-    for (int i = 1; i <= 1166; i++) {
+    for (int i = 1; i <= 1165; i++) {
         printf("%d\t|%lf\n", i, IsCycle == 1 ? countWaysCycle(i): countWaysRecursive(i));
     }
 	printf("\n");
